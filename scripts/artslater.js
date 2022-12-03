0c78a7bb07c0e3f27d5096a1fa8b8b91eda48529
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-02 21:27:54 -0800

    debugging json

---
+++ scripts/artslater.js
@@ -0,0 +1,60 @@
+// Generates Slates to put on the Artbook Page
+
+// Slate root
+const topLevel = document.querySelector(".galleryRoot");
+
+// create new Slate
+function gallerySlate(slate, name, images, description, tags) {
+    slate.classList.add("galleryDiv");
+
+    let lhs = document.createElement("div");
+    let rhs = document.createElement("div");
+    rhs.classList.add("rightDiv");
+    lhs.classList.add("leftDiv");
+
+    for (var i = 0; i < images.length; i++) {
+        let imgdiv = document.createElement("div");
+        let image = document.createElement("img");
+        image.classList.add("galleryImg");
+        image.setAttribute("src", images[i].source);
+        image.setAttribute("alt", "image missing");
+        imgdiv.append(image);
+        lhs.append(imgdiv);
+    }
+
+    let header = document.createElement("h2");    
+
+    let desc = document.createElement("p");
+    desc.textContent = description;
+
+    let tag = document.createElement("p");
+    tag.classList.add("tags");
+    tag.textContent = tags;
+
+    slate.append(lhs);
+    slate.append(rhs);
+    rhs.append(header);
+    rhs.append(desc);
+    rhs.append(tag);
+}
+
+var loadGalleryslates = await fetch("../structures/wallslates.json");
+var galleryslates = loadWallslates.json();
+
+var slates = galleryslates.pieces;
+
+for (var i = 0; i < slates.length; i++) {
+    let sName = slates[i].name;
+    let sImages = slates[i].images;
+    let sDesc = slates[i].description;
+    let sTags = slates[i].tags;
+
+    var newSlate = document.createElement("div");
+    gallerySlate(newSlate,
+                    sName,
+                    sImages,
+                    sDesc,
+                    sTags);
+    
+    topLevel.appendChild(newSlate);
+}
---
+++ scripts/wallslater.js
@@ -0,0 +1,61 @@
+// Generates Slates to put on the Jericho Wall
+
+// Slate root
+const topLevel = document.querySelector(".slateRoot");
+
+// create new Slate
+function templateSlate(slate, name, short, link, description, tags) {
+    slate.classList.add("entryDiv");
+
+    let rhs = document.createElement("div");
+    rhs.classList.add("rightDiv");
+
+    let header = document.createElement("h2");
+    let headerLink = document.createElement("a");
+    headerLink.setAttribute("href", link);
+    headerLink.textContent = name;    
+
+    let desc = document.createElement("p");
+    desc.textContent = description;
+
+    let tag = document.createElement("p");
+    tag.classList.add("tags");
+    tag.textContent = tags;
+
+    slate.append(rhs);
+    rhs.append(header);
+    header.append(headerLink);
+    header.innerHTML += short;
+    rhs.append(desc);
+    rhs.append(tag);
+}
+
+
+// parse out json
+var loadWallslates = await fetch("../structures/wallslates.json");
+var wallslates = loadWallslates.json();
+
+var slates = wallslates.entries;
+
+for (var i = 0; i < slates.length; i++) {
+        let sName = slates[i].name;
+        let sShort = slates[i].short;
+        let sLink = slates[i].source;
+        let sDesc = slates[i].description;
+        let sTags = slates[i].tags;
+
+        var newSlate = document.createElement("div");
+        templateSlate(newSlate,
+                        sName,
+                        sShort,
+                        sLink,
+                        sDesc,
+                        sTags);
+        
+        topLevel.appendChild(newSlate);
+}
+
+
+
+
+

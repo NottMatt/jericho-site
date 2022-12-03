3b1588c3b6896fae4c815cd9bc1168a4f78fe0fe
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-03 15:34:26 -0800

    update

--- scripts/todayslater.js
+++ scripts/todayslater.js
@@ -1,7 +1,7 @@
 // Generates Slates to put on the Today
 
 const based = "https://en.wikipedia.org/wiki/Human_radiation_experiments";
-const cringe = "https://en.wikipedia.org/wiki/British_royal_family";
+const cringe = "https://en.wikipedia.org/wiki/Alabama_Historical_Society";
 
 const bF = document.querySelector(".basedFrame");
 const cF = document.querySelector(".cringeFrame");

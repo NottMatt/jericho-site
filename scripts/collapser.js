694107403b0ad03fb3352fa54069039fcbb5ce5e
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-03 23:20:38 -0800

    pixels

--- scripts/collapser.js
+++ scripts/collapser.js
@@ -6,10 +6,10 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      if (content.style.maxHeight = 0){
+      if (content.style.maxHeight = "0px"){
         content.style.maxHeight = content.scrollHeight + "px";
       } else {
-        content.style.maxHeight = 0;
+        content.style.maxHeight = "0px";
       }
     });
   }

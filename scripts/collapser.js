b885c439a72948ccac57ef42b27711855e6e8c94
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-03 23:25:36 -0800

    js stuff

--- scripts/collapser.js
+++ scripts/collapser.js
@@ -6,6 +6,7 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
+
       if (content.style.maxHeight){
         content.style.maxHeight = null;
       } else {

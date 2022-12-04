b0bc81b058809d3c7ef7be1c53cc96a7b194bfa6
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-03 23:12:07 -0800

    adding collapsing slates

---
+++ scripts/collapser.js
@@ -0,0 +1,15 @@
+// makes a region collapsible
+var coll = document.getElementsByClassName("collapser");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }

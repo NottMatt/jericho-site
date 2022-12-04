4ceb728c3582c7e446d9e80d7ed08e31334e2c7d
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-03 23:21:57 -0800

    going back to what worked

--- scripts/collapser.js
+++ scripts/collapser.js
@@ -6,10 +6,10 @@ for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      if (content.style.maxHeight = "0px"){
-        content.style.maxHeight = content.scrollHeight + "px";
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
       } else {
-        content.style.maxHeight = "0px";
+        content.style.maxHeight = content.scrollHeight + "px";
       }
     });
   }

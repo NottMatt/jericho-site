a77dd1218048a13c5d99902a456fa15ed3a42930
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-06 11:03:20 -0800

    update

--- scripts/todayslater.js
+++ scripts/todayslater.js
@@ -1,7 +1,7 @@
 // Generates Slates to put on the Today
 
 const based = "https://en.wikipedia.org/wiki/Suicide_door";
-const cringe = "https://en.wikipedia.org/wiki/Marriage_(Wales)_Act_2010";
+const cringe = "https://en.wikipedia.org/wiki/Turf_maze";
 
 const bF = document.querySelector(".basedFrame");
 const cF = document.querySelector(".cringeFrame");

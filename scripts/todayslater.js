3b62bc5a9dc38604e0ce76b0ec1f7c78290ceaae
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-07 12:49:51 -0800

    update today

--- scripts/todayslater.js
+++ scripts/todayslater.js
@@ -1,7 +1,7 @@
 // Generates Slates to put on the Today
 
-const based = "https://en.wikipedia.org/wiki/Suicide_door";
-const cringe = "https://en.wikipedia.org/wiki/Turf_maze";
+const based = "https://en.wikipedia.org/wiki/J._Robert_Oppenheimer";
+const cringe = "https://en.wikipedia.org/wiki/Tour_de_France";
 
 const bF = document.querySelector(".basedFrame");
 const cF = document.querySelector(".cringeFrame");

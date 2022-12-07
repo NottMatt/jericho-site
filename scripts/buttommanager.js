87d44f208ea5273651f416de486a4e3739470feb
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-06 19:09:53 -0800

    buttons

---
+++ scripts/buttommanager.js
@@ -0,0 +1,11 @@
+const commentButton = document.querySelector(".comment");
+const hatemailButton = document.querySelector(".hatemail");
+
+hatemailButton.addEventListener("click", function() {
+    window.alert("get a friend");
+});
+
+commentButton.addEventListener("click", function() {
+    window.alert("talk to the hand");
+});
+

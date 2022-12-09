fd9792f9061c82800f6ae09035eea605261032ba
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-09 12:35:59 -0800

    update today

--- scripts/todayslater.js
+++ scripts/todayslater.js
@@ -1,7 +1,7 @@
 // Generates Slates to put on the Today
 
-const based = "https://en.wikipedia.org/wiki/J._Robert_Oppenheimer";
-const cringe = "https://en.wikipedia.org/wiki/Tour_de_France";
+const based = "https://en.wikipedia.org/wiki/Mongol_invasions_and_conquests";
+const cringe = "https://en.wikipedia.org/wiki/Essential_oil";
 
 const bF = document.querySelector(".basedFrame");
 const cF = document.querySelector(".cringeFrame");

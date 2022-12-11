78f83d45e113081b08392d6be8ff8cd05946e9f0
Matthew Tiemersma <mtiemersma19@georgefox.edu>
2022-12-11 01:40:55 -0800

    update today

--- scripts/todayslater.js
+++ scripts/todayslater.js
@@ -1,7 +1,7 @@
 // Generates Slates to put on the Today
 
-const based = "https://en.wikipedia.org/wiki/Mongol_invasions_and_conquests";
-const cringe = "https://en.wikipedia.org/wiki/Essential_oil";
+const based = "https://en.wikipedia.org/wiki/F.J._Fuchs";
+const cringe = "https://en.wikipedia.org/wiki/Proposed_Japanese_invasion_of_Sichuan";
 
 const bF = document.querySelector(".basedFrame");
 const cF = document.querySelector(".cringeFrame");

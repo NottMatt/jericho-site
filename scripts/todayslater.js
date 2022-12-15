428a3a0c834bed5dfd61281f388e10566aabbd4c
NottMatt <mattdtie@gmail.com>
2022-12-15 23:55:29 +0000

    many changes

--- scripts/todayslater.js
+++ scripts/todayslater.js
@@ -1,7 +1,7 @@
 // Generates Slates to put on the Today
 
-const based = "https://en.wikipedia.org/wiki/Graph_reduction";
-const cringe = "https://en.wikipedia.org/wiki/Holacanthus";
+const based = "https://en.wikipedia.org/wiki/Royal_Canadian_Curling_Club";
+const cringe = "https://en.wikipedia.org/wiki/John_Cena";
 
 const bF = document.querySelector(".basedFrame");
 const cF = document.querySelector(".cringeFrame");
